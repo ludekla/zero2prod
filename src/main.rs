b5f5138cda5ac6db71e2dc0276a113aa10412a5c
Lutz <ludewitz@gmx.de>
2024-03-31 23:29:01 +0200

    Finish §3.5 with test endpoint.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use zero2prod::run;
+use std::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:0")
+        .expect("Failed ot bind random port");
+    run(listener)?.await
 }
